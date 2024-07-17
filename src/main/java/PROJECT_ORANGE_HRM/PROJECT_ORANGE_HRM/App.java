1288bc5f38dadd04657679572b7b9e10c9a04925
Kundan Patel <knp.patel18@gmail.com>
2024-07-17 08:13:56 +0530

    Project created by team leader

---
+++ src/main/java/PROJECT_ORANGE_HRM/PROJECT_ORANGE_HRM/App.java
@@ -0,0 +1,13 @@
+package PROJECT_ORANGE_HRM.PROJECT_ORANGE_HRM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
